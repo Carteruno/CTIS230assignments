b021e36ef464ac0ac47bce65c4ba739f61ee4b36
Carteruno <cwebb1@guilford.edu>
2025-03-13 18:34:12 -0400

    mouseout function works

---
+++ first website project/testscript.js
@@ -0,0 +1,23 @@
+//myitem is defined by the document element and the getElementById method.
+//Any element with the id "firsttest" will return the given element.
+{myitem = document.getElementById("firsttest");
+//an event listener adds an event handler, in this case the event handler
+//is being added to the myitem element by the .addEventListener Method.
+//"click" is the name given to event and "Onclick" is the Method. 
+    myitem.addEventListener("click", onClick);
+    myitem.addEventListener("mouse off", onmouseout);
+//This code states when the onclick function/Method is invoked, the element returned to
+//myitem tagged with "firsttest" id will invoke an event. This event will execute myitem.style.color = "green"
+//The event of the OnClick function will turn the given element's text green.
+
+function onClick() {  
+        myitem.style.color = "green"; 
+        myitem.style.fontSize = "25px";
+    }
+
+    function onmouseout() {
+        myitem.style.color = "";
+        myitem.style.fontSize = "";
+    }
+}
+    
