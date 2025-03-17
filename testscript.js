f9e177a1e61edd5d453f63f2247ed893ce0949de
Carteruno <cwebb1@guilford.edu>
2025-03-17 19:42:22 -0400

    commit project 6

--- testscript.js
+++ testscript.js
@@ -4,8 +4,10 @@
     myitem = document.getElementById("firsttest");
     thebutton = document.getElementById("thebutton");
     thesecondbutton = document.getElementById("thesecondbutton")
+    thethirdButton = document.getElementById("thethirdbutton")
     otheritem = document.getElementById("buttontest");
     secondotheritem = document.getElementById("secondbuttontest");
+    thirditem = document.getElementById("thirdbuttontest")
     //an event listener adds an event handler, in this case the event handler
     //is being added to the myitem element by the .addEventListener Method.
     //"click" is the name given to event and "Onclick" is the Method. 
@@ -13,6 +15,7 @@
     myitem.addEventListener("mouse off", onmouseout);
     thebutton.addEventListener("click", onButtonClick1);
     thesecondbutton.addEventListener("click",onButtonClick2);
+    thethirdbutton.addEventListener("click",onButtonClick3);
     //This code states when the onclick function/Method is invoked, the element returned to
     //myitem tagged with "firsttest" id will invoke an event. This event will execute myitem.style.color = "green"
     //The event of the OnClick function will turn the given element's text green.
@@ -34,6 +37,9 @@
     function onButtonClick2() {
         secondotheritem.style.color ="blue";
     }
+    function onButtonClick3() {
+        thirditem.style.color = "orange";
+    } 
 
     textentry = document.getElementById("myinput");  
     textentry.addEventListener("change", onChange);
